e99c4a50d7b4f3d933ae9a9e1d97e7c354876935
Igor Nazirov <mettbs@gmail.com>
2023-04-27 03:02:27 +1100

    Popup menu and js interactivity

---
+++ src/components/data.js
@@ -0,0 +1,39 @@
+import img1 from '../images/_0000_1080X1350.jpg';
+import img2 from '../images/_0000_1080X1350.jpg';
+import img3 from '../images/_0004_1080X135018.jpg';
+import img4 from '../images/_0005_1080X135017.jpg';
+import img5 from '../images/_0006_1080X135016.jpg';
+import img6 from '../images/_0007_1080X135015.jpg';
+import img7 from '../images/_0008_1080X135014.jpg';
+import img8 from '../images/_0009_1080X135013.jpg';
+import img9 from '../images/_0010_1080X135012.jpg';
+import img10 from '../images/_0011_1080X135011.jpg';
+import img11 from '../images/_0013_1080X13509.jpg';
+import img12 from '../images/_0014_1080X13508.jpg';
+import img13 from '../images/_0015_1080X13507.jpg';
+import img14 from '../images/_0016_1080X13506.jpg';
+import img15 from '../images/_0018_1080X13504.jpg';
+import img16 from '../images/_0019_1080X13503.jpg';
+import img17 from '../images/_0020_1080X13502.jpg';
+
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+];
+
+export { images };
---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+const menuSelectors = {
+  popup: '.popup-menu',
+  visible: 'popup-menu_visible',
+  closeButton: '.popup-menu__close-button',
+  openButton: '.header__menu-button',
+};
+
+const cardSelectors = {
+  container: '.looks__image-grid',
+  card: '.looks__image-card',
+  fade: 'looks__image-card_fade',
+};
+
+const fadeTime = 500;
+const changeInterval = 300;
+const changeAmount = 3;
+
+export { menuSelectors, cardSelectors, fadeTime, changeInterval, changeAmount };
